8af4c8e6f362358964eb7d8a60e902de18860bf4
Maksim Frankov <2m.franko@gmail.com>
2022-12-27 23:36:25 +0200

    4th css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -5,18 +5,18 @@ function TransactionHistory({ items }) {
     return (
         <table className={css.transaction_history}>
   <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+    <tr className={css.tableRow}>
+      <th className={css.tableHead}>Type</th>
+      <th className={css.tableHead}>Amount</th>
+      <th className={css.tableHead}>Currency</th>
     </tr>
   </thead>
             {items.map(items =>
-                <tbody key={items.id}>
-                    <tr>
-                        <td>{items.type}</td>
-                        <td>{items.amount}</td>
-                        <td>{items.currency}</td>
+                <tbody key={items.id} className={css.table}>
+                    <tr className={css.tableRow}>
+                        <td className={css.tableBody}>{items.type}</td>
+                        <td className={css.tableBody}>{items.amount}</td>
+                        <td className={css.tableBody}>{items.currency}</td>
                     </tr>
                 </tbody>
 )}
